7499c96f9fc7546aeb1808129622a6f90b961a62
assettasimone <assettasimone@gmail.com>
2025-09-23 00:11:28 +0200

    Add Header component and integrate it into App

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import logo from "../assets/img/dc-logo.png"
+
+export default function () {
+    return (
+        <header className="flex justify-between max-w-7xl mx-auto my-6">
+
+            <img src={logo} alt="dc-logo" className="w-22 h-22" />
+            <nav>
+                <ul className="h-full flex justify-items-center">
+                    <li className="px-4 content-center">characters</li>
+                    <li className="px-4 content-center">comics</li>
+                    <li className="px-4 content-center">movies</li>
+                    <li className="px-4 content-center">tv</li>
+                    <li className="px-4 content-center">games</li>
+                    <li className="px-4 content-center">collectibles</li>
+                    <li className="px-4 content-center">videos</li>
+                    <li className="px-4 content-center">fans</li>
+                    <li className="px-4 content-center">news</li>
+                    <li className="px-4 content-center">shop</li>
+                </ul>
+            </nav>
+
+
+        </header>
+    )
+}
